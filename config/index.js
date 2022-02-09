4b2bc1415d2a8e04e21f3f1e37fe6e2321ffd3af
vinoth-berkmons <vinothberkmons@gmail.com>
2022-02-10 00:48:34 +0530

    basic setup

---
+++ config/index.js
@@ -0,0 +1,7 @@
+const dotenv = require('dotenv');
+
+function loadConfig() {
+    dotenv.config();
+}
+
+module.exports = loadConfig;
---
+++ services/index.js
@@ -0,0 +1,7 @@
+const students = require('./students');
+const courses = require('./courses');
+
+module.exports = {
+    students,
+    courses,
+}
