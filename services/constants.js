3a6f754ef91c477a6379544db15abf97237ea794
vinoth-berkmons <vinothberkmons@gmail.com>
2022-02-10 09:29:42 +0530

    db constants

---
+++ services/constants.js
@@ -0,0 +1,9 @@
+const MAIN_DATABASE = 'academia';
+const COURSES_COLLECTION = 'courses';
+const STUDENTS_COLLECTION = 'students';
+
+module.exports = {
+    MAIN_DATABASE,
+    COURSES_COLLECTION,
+    STUDENTS_COLLECTION,
+};
